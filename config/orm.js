f863d5d4e70f3a232779e4ead0453835b97b288a
Paul Panchukov <panchukovp@gmail.com>
2019-11-11 12:13:46 -0500

    added burgers to handlebars index page

--- config/orm.js
+++ config/orm.js
@@ -11,15 +11,37 @@ function printQuestionMarks(num) {
     return arr.toString();
   }
 
+  function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+
 
 var orm = {
     all: function(tableInput, cb){
-        var queryString = "SELECT burger_name FROM ??";
+        var queryString = "SELECT * FROM ??";
 connection.query(queryString, [tableInput], function(err,result){
     if(err) {
         throw err;
     }
-
+console.log(result)
     cb(result);
 });
     },
@@ -37,9 +59,27 @@ connection.query(queryString, [tableInput], function(err,result){
         if(err) {
             throw err
         }
+        console.log(results)
         cb(results)
     })
-    }
+    },
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
 }
 
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,10 +6,10 @@ var burger = require("../models/burgers.js");
 
 router.get("/", function(req, res){
     burger.all(function(data){
-res.render("index", {burger: data})
+res.render("index", {burgers: data})
     })
 });
-router.post("/index", function(req, res){
+router.post("/api/burgers", function(req, res){
     burger.create([
         "burger_name", "devoured"
     ],[
@@ -17,6 +17,22 @@ router.post("/index", function(req, res){
     ], function(result){
         res.json({id: result.insertId});
     })
-})
+});
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      sleepy: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 module.exports = router;
