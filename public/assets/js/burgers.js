14689b1ba5cd6c59b017ad19737d26446ad001f5
Paul Panchukov <panchukovp@gmail.com>
2019-11-13 11:55:46 -0500

    made changes to UI

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 $(function() {
 $(".addBurger").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+
     event.preventDefault();
 
     var newBurger = {
@@ -8,14 +8,14 @@ $(".addBurger").on("submit", function(event) {
       devoured: 0
     };
 
-    // Send the POST request.
+
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
+
         location.reload();
       }
     );
@@ -33,7 +33,7 @@ $(".devour").on("click", function(){
     }).then(
       function() {
         console.log("changed devour to", newstate);
-        // Reload the page to get the updated list
+
         location.reload();
       }
     );
